419be543ed6090f104c1476483d132aed94a0b29
Irena Chen <irena@macbook-air>
2021-02-07 23:49:41 -0800

    build calculateDuration function

--- 02062021-OOP practice/index.js
+++ 02062021-OOP practice/index.js
@@ -1,24 +1,44 @@
 function Stopwatch(){
-    this.duration = 0;
     let startflag = 0;
-    let currentTime;
-    const duraFormat = function(){
-
+    let startTime, endTime;
+    const calculateDuration = function(){
+        return (endTime-startTime)/1000;  
     }
     
     this.start = function(){
         if (startflag === 1){
             throw new Error("Stopwatch has already started!")
         }else{
-            currentTime = new Date().getTime();
+            startTime = new Date().getTime();
             startflag = 1;
         }
         
     };
+    
     this.stop = function(){
-
+        if (startflag === 1){
+            endTime = new Date().getTime();
+            startflag = 0;
+        }else{
+            throw new Error("Stopwatch is not started yet.")
+        }
     };
+
     this.reset = function(){
-
+        this.duration = 0;
     };
+
+
+    Object.defineProperty(this,'duration',{
+        get: function(){
+            typeof startTime
+            if (startTime && endTime){
+                return calculateDuration()
+            }else if(startTime){
+                throw new Error("Timer is on-going!")
+            }else{
+                return 0
+            }
+        }
+    })
 }
