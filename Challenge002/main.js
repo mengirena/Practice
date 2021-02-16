374649338b3192dedf4fa44cdc088965ce8c4f4a
Irena-iMac <irena1009@gmail.com>
2021-02-15 17:17:59 -0800

    use if...else to solve the problem

---
+++ Challenge002/main.js
@@ -0,0 +1,28 @@
+function zero(index) {
+    return 
+}
+function one(chain) {
+    if(!chain){
+        return "1"
+    } else{
+        return eval("1"+ chain)
+    }
+}
+function two() {}
+function three() {}
+function four() {}
+function five() {}
+function six() {}
+function seven() {}
+function eight() {}
+function nine() {}
+
+function plus(num) {
+    return "+" + num
+}
+
+function minus() {}
+function times() {}
+function dividedBy() {}
+
+
