22404209aaec16afa2a742e8c66a14f429757740
Irnea Chen <irena1009@gmail.com>
2021-02-25 23:10:18 -0800

    s1)add tyling 2)add the stop add more X and O after the winner is identified 3)add setTimeout to refresh the board

--- 100devs-tictoctoe/main.js
+++ 100devs-tictoctoe/main.js
@@ -6,24 +6,24 @@ class Board{
   
     markChoice(e){
       let id = e.target.id
-      
       let selectedBox = document.querySelector(`#${id}`)
       if (selectedBox.innerHTML == ""){
-        let choice = (this.flag === 1 )? "O":"X"
+        let choice = (this.flag === 1)? "O": (this.flag === -1)? "X": ""
         selectedBox.innerText = choice
         this.userChoice[Math.floor(parseInt(id[1])/3)][parseInt(id[1])%3]= choice
-        console.log(this.userChoice)
         let verifiedResult = this.winChecker()
-        console.log("verifiedResult", verifiedResult)
+        this.flag *= -1;
         if (verifiedResult !== "") {
           document.querySelector("span").innerText = verifiedResult + " wins!"
+          this.userChoice = [['','',''],['','',''],['','','']];
+          this.flag = 0;
           setTimeout(()=>{
             document.querySelectorAll("div").forEach(item=>item.innerText="")
             document.querySelector("span").innerText = ""
             this.userChoice = [['','',''],['','',''],['','','']];
+            this.flag = 1
           },3000)
         }
-        this.flag *= -1;
       }
     }
   
@@ -31,23 +31,21 @@ class Board{
       //rows
       for(let i = 0; i<3;i++){
         if (this.userChoice[i][0]=== this.userChoice[i][1] && this.userChoice[i][1]=== this.userChoice[i][2] && this.userChoice[i][0]!=="") {
-          console.log(`${this.userChoice[i][0]} ${this.userChoice[i][1]} ${this.userChoice[i][2]}`)
-          return this.userChoice[i][2]
+            return this.userChoice[i][2]
         }
       }
       //columns
       for(let j = 0; j<3; j++){
         if (this.userChoice[0][j]=== this.userChoice[1][j] && this.userChoice[1][j]=== this.userChoice[2][j] && this.userChoice[0][j]!=="") {
-          console.log(`${this.userChoice[0][j]} ${this.userChoice[1][j]} ${this.userChoice[2][j]}`)
-          return this.userChoice[2][j]
+            return this.userChoice[2][j]
         }
       }
       //diagonals
       if (this.userChoice[0][0]=== this.userChoice[1][1] && this.userChoice[1][1]=== this.userChoice[2][2]){
-        return this.userChoice[2][2]
+          return this.userChoice[2][2]
       } 
       if (this.userChoice[0][2]=== this.userChoice[1][1] && this.userChoice[1][1]=== this.userChoice[2][0]){
-        return this.userChoice[2][0]
+          return this.userChoice[2][0]
       }
       return ""
     }
