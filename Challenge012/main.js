572911ab281fbc4d05e19eb78caf715cef11e7e7
Irnea Chen <irena1009@gmail.com>
2021-03-04 14:57:21 -0800

    create Ch012

---
+++ Challenge012/main.js
@@ -0,0 +1,9 @@
+function SnakesLadders() {
+    this.transferPoints  = [2 ,7 ,8 ,15,16,21,28,36,46,49,51,62,64,71,74,78,87,89,92,95,99];
+    this.transferActions = [38,14,31,26,6 ,42,84,44,25,11,67,19,60,91,53,98,94,68,88,75,80];
+    
+};
+
+SnakesLadders.prototype.play = function(die1, die2) {
+
+}
--- Eloquent JS/main.js
+++ Eloquent JS/main.js
@@ -1,5 +1,37 @@
-function range(startNum, endNum) {
-    for (;;) {
+function arrayToList(array){
+  let rArr = array.reverse()
+  let first = {}
+  let rest = {}
+  for (let i = 0 ; i < rArr.length; i++){
+    rest.value = rArr[i]
+    if (i === 0) rest.rest = null
+    if(i !== rArr.length -1){
+      first.value = ""
+      first.rest = rest
+      rest = first
     }
   }
-  
+  return first
+}
+
+function listToArray(){
+
+}
+
+function prepend(){
+
+}
+
+function nth(){
+
+}
+
+
+console.log(arrayToList([10, 20, 30]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
