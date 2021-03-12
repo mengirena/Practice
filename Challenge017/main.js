51942301b7823ea1dcf39f66c98856124db011be
Irena Chen <irena1009@gmail.com>
2021-03-12 01:28:40 -0800

    fill board function

--- Challenge017/main.js
+++ Challenge017/main.js
@@ -5,9 +5,42 @@ Connect 4
 
 */
 function Connect4 (){
-    // Code Here
+    this.player = 1
+    this.board = [[0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0]]
 };
   
 Connect4.prototype.play = function (col){
-    // Code Here
+    let i = 5
+    while(i>=0){ 
+        if(this.board[i][col] === 0){
+            this.board[i][col] = this.player
+            console.log(`Player${this.player} has a turn`)
+            
+            break;
+        }
+        i--
+    }
+    return `${i}-${col}`
 };
+
+function checkConnection(i,col){
+
+}
+/*
+[    [0,1,2,3,4,5,6]
+    0[0,0,0,0,0,0,0], 
+    1[0,0,0,0,0,0,0],
+    2[0,0,0,0,0,0,0],
+    3[0,0,0,0,0,0,0],
+    4[0,0,0,0,0,0,0],
+    5[0,0,0,0,0,0,0]
+]
+*/
+game = new Connect4();
+console.log(game.play(0))
+console.log(game.play(0))
