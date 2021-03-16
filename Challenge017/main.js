04192c0714589a068133bc72f2d4884964589928
Irena Chen <irena1009@gmail.com>
2021-03-15 23:30:56 -0700

    finish winner checker

--- Challenge017/main.js
+++ Challenge017/main.js
@@ -12,43 +12,89 @@ function Connect4 (){
                   [0,0,0,0,0,0,0],
                   [0,0,0,0,0,0,0],
                   [0,0,0,0,0,0,0]]
-};
-  
-Connect4.prototype.play = function (col){
-    let i = 5
-    
-    while(i>=0){ 
-        if(this.board[i][col] === 0){
-            this.board[i][col] = this.player
-            checkCategory(i,col)
-            console.log(`Player ${this.player} has a turn`)
-            break;
-        }
-        i--
-    }
-    this.player = (this.player === 1) ? 2 : 1
-    return `${i}-${col}`
-};
-
-function checkCategory(i,col){
-    let section = [["a","a","a","b","c","c","c"],
+    this.category = [["a","a","a","b","c","c","c"],
                     ["a","a","a","b","c","c","c"],
                     ["a","a","a","b","c","c","c"],
                     ["d","d","d","e","f","f","f"],
                     ["d","d","d","e","f","f","f"],
                     ["d","d","d","e","f","f","f"]]
-    return section[i][col]
+};
+  
+Connect4.prototype.play = function (col){
+    let i = 5
+    let result = "" , returnStr = ""
+    let checkType = {
+        a:[right,down,rdDiag],
+        b:[left,ldDiag,down,rdDiag,right],
+        c:[left,ldDiag,down],
+        d:[up,ruDiag,right],
+        e:[left,luDiag,up,ruDiag,right],
+        f:[left,luDiag,up]
+    }
+    while(i >= 0){ 
+        if(this.board[i][col] === 0){
+            this.board[i][col] = this.player
+            result = checkType[this.category[i][col]].reduce((result,checker) => {
+                if (checker(this,i,col)) {
+                    result = true
+                }
+                return result
+            },"")
+            break;
+        }
+        i--
+    }
+    returnStr = (!result) ? `Player ${this.player} has a turn` : `Player ${this.player} wins!`
+    this.player = (this.player === 1) ? 2 : 1
+    return returnStr
+};
+
+function right(obj,i,col){
+    return obj.board[i][col] == obj.board[i][col+1] &&
+           obj.board[i][col+1] == obj.board[i][col+2] &&
+           obj.board[i][col+2] == obj.tboard[i][col+3]
 }
-/*
-[    [0,1,2,3,4,5,6]
-    0[0,0,0,0,0,0,0], 
-    1[0,0,0,0,0,0,0],
-    2[0,0,0,0,0,0,0],
-    3[0,0,0,0,0,0,0],
-    4[0,0,0,0,0,0,0],
-    5[0,0,0,0,0,0,0]
-]
-*/
+function left(obj,i,col){
+    return obj.board[i][col] == obj.board[i][col-1] &&
+           obj.board[i][col-1] == obj.board[i][col-2] &&
+           obj.board[i][col-2] == obj.board[i][col-3]
+}
+function down(obj,i,col){
+    return obj.board[i][col] == obj.board[i+1][col] &&
+           obj.board[i+1][col] == obj.board[i+2][col] &&
+           obj.board[i+2][col] == obj.board[i+3][col]
+}
+function up(obj,i,col){
+    return obj.board[i][col] == obj.board[i-1][col] &&
+           obj.board[i-1][col] == obj.board[i-2][col] &&
+           obj.board[i-2][col] == obj.board[i-3][col]
+}
+function ruDiag(obj,i,col){
+    return obj.board[i][col] == obj.board[i-1][col+1] &&
+           obj.board[i-1][col+1] == obj.board[i-2][col+2] &&
+           obj.board[i-2][col+2] == obj.board[i-3][col+3]
+}
+function luDiag(obj,i,col){
+    return obj.board[i][col] == obj.board[i-1][col+1] &&
+           obj.board[i-1][col+1] == obj.board[i-2][col+2] &&
+           obj.board[i-2][col+2] == obj.board[i-3][col+3]
+}
+function rdDiag(obj,i,col){
+    return obj.board[i][col] == obj.board[i+1][col-1] &&
+           obj.board[i+1][col-1] == obj.board[i+2][col-2] &&
+           obj.board[i+2][col-2] == obj.board[i+3][col-3]
+}
+function ldDiag(obj,i,col){
+    return obj.board[i][col] == obj.board[i+1][col-1] &&
+           obj.board[i+1][col-1] == obj.board[i+2][col-2] &&
+           obj.board[i+2][col-2] == obj.board[i+3][col-3]
+}
+
 game = new Connect4();
 console.log(game.play(0))
+console.log(game.play(1))
+console.log(game.play(0))
+console.log(game.play(1))
+console.log(game.play(0))
+console.log(game.play(1))
 console.log(game.play(0))
