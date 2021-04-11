b39ed6a4fc7524ce4d84a2d834f972b5a5f10e10
Irena Chen <irena1009@gmail.com>
2021-04-11 16:19:19 -0700

    connect DB, structure routes

---
+++ multer-cloudinary/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config(); 
+
+// Connect DB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("mongoDB is connected"))
+  .catch((err) => console.log(err)); 
+
+// Middleware
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Route
+app.use('/', require('./routes/home'))
+app.use('/user', require('./routes/user'))
+
+app.listen(5000, () => console.log("Server is running"));
