eb7d688766c7c4b207b4695f7a7cede647e40813
Irena Chen <irena1009@gmail.com>
2021-04-19 18:09:37 -0700

    add multi rappers

--- Rap-Api/server.js
+++ Rap-Api/server.js
@@ -1,19 +1,41 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 8000
 
-let savage = {
+app.use(cors())
+
+let rappers = {
+    '21 savage':{
     'age': 28,
-    'birthName': 'Sheyaa',
+    'birthName': 'Sheyaa Bin Abraham-Joseph',
     'birthLocation': 'London, England'
+    },
+    'chance the rapper':{
+        'age': 27,
+        'birthName': 'Chancelor Jonathan Bennett',
+        'birthLocation': 'Chicago, Illionis'
+    },
+    'Unknown':{
+        'age': 0,
+        'birthName': 'unknown',
+        'birthLocation': 'unknown'
+    }
 }
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api/savage', (req, res)=>{
-    res.json(savage)
+app.get('/api/rappers/:rapperName', (req, res)=>{
+    const rapName = req.params.rapperName.toLowerCase()
+    console.log(rapName)
+    if (rappers[rapName]){
+        res.json(rappers[rapName])
+    }else{
+        res.json(rappers['dylan'])
+    }
+    
 })
 
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
