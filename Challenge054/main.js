ee9f15c7844bda3c5013ba0fbf97feb947be554e
Irnea Chen <irena1009@gmail.com>
2021-05-17 20:57:42 -0700

    6 kyu-Sum of Digits / Digital Root

---
+++ Challenge054/main.js
@@ -0,0 +1,27 @@
+/*
+Sum of Digits / Digital Root
+Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+Examples
+
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+*/
+
+function digital_root(n) {
+    while (n >= 10){
+       n = `${n}`.split("").reduce((sum,value) => Number(sum) + Number(value))
+    }
+    return n
+}
+
+console.log(digital_root(10))
+
+//other's method
+function digital_root(n) {
+    return (n - 1) % 9 + 1;
+  }
