aa03f980d987459760cd509b645f7e2a46e109f5
Irena Chen <irena1009@gmail.com>
2021-06-12 11:44:02 -0700

    add fake data

---
+++ react-todo-list/src/todoData.js
@@ -0,0 +1,27 @@
+export default todoData = [
+    {
+        id: 1,
+        item: "Get oat milk",
+        completed: false,
+    },
+    {
+        id: 2,
+        item: "Get banana",
+        completed: false,
+    },
+    {
+        id: 3,
+        item: "Get strawberry",
+        completed: true,
+    },
+    {
+        id: 4,
+        item: "Get pencil",
+        completed: false,
+    },
+    {
+        id: 5,
+        item: "Get egg",
+        completed: true,
+    },
+]
