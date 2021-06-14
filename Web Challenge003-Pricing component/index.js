866c7aa259be159a9345ddfd1b820db912af933c
Irena Chen <irena1009@gmail.com>
2021-06-14 14:40:17 -0700

    add eventlistener for each card for click and mouseleave

--- Web Challenge003-Pricing component/index.js
+++ Web Challenge003-Pricing component/index.js
@@ -1,1 +1,28 @@
-// 🐱 🦁 🐯
+const cardClick = Array.from(document.querySelectorAll(".card"))
+const planClick = Array.from(document.querySelector(".plan"))
+
+cardClick.forEach(card => card.addEventListener("click", enlargeTheClicked))
+cardClick.forEach(card => card.addEventListener("mouseleave", removeEnlarge))
+
+
+function enlargeTheClicked(){
+    let cards = ['eco', 'classic', 'lux']
+    cards.forEach(card=>{
+        if (card == this.id){
+            this.classList.toggle("emphasis")
+        }else{
+            document.querySelector(`#${card}`).classList.toggle("defocus")
+        }
+    })
+}
+
+function removeEnlarge(){
+    let cards = ['eco', 'classic', 'lux']
+    cards.forEach(card=>{
+        if (card == this.id){
+            this.classList.remove("emphasis")
+        }else{
+            document.querySelector(`#${card}`).classList.remove("defocus")
+        }
+    })
+}
