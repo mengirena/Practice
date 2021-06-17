64faad8e2ff10806c1a24c5786c587d8a57d436e
Irena Chen <irena1009@gmail.com>
2021-06-16 22:36:36 -0700

    add split function

--- Web Challenge004-Tip calculator/script.js
+++ Web Challenge004-Tip calculator/script.js
@@ -5,14 +5,21 @@ const splitValue = document.querySelector("#head_count")
 
 billValue.addEventListener("change", changeTotal)
 tipValue.addEventListener("change", changeTotal)
+splitValue.addEventListener("change", updateSplit)
+
+let total
 
 function changeTotal(){
     const bill = Number(billValue.value)
     const tip = Number(tipValue.value)
-    const total = Math.round((bill*(1+tip/100))*100)/100
+    total = Math.round((bill*(1+tip/100))*100)/100
     const temp = total.toString().split(".")
-    document.querySelector("#total").innerHTML = `${(temp.length == 1) ? `${total}.00` : (temp[1].length === 1) ? `${total}0` : total}`
+    document.querySelector("#total").innerHTML = `${(temp.length == 1) ? `$${total}.00` : (temp[1].length === 1) ? `$${total}0` : `$${total}`}`
 
-    splitBox.style.height = "10%"
-    
+    splitBox.style.height = "20%"
+    splitValue.value && updateSplit()
+}
+
+function updateSplit(){
+    document.querySelector("#split").innerHTML = `<strong>$${total/splitValue.value}</strong>`
 }
