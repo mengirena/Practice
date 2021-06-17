0e72f75ad0fba6e780891288adafa9f0ffd52b4a
Irena Chen <irena1009@gmail.com>
2021-06-16 23:23:38 -0700

    add round with zero

--- Web Challenge004-Tip calculator/script.js
+++ Web Challenge004-Tip calculator/script.js
@@ -2,24 +2,48 @@ const billValue = document.querySelector("#bill_value")
 const tipValue = document.querySelector("#tip_value")
 const splitBox = document.querySelector(".pop_up")
 const splitValue = document.querySelector("#head_count")
+const roundBtn = document.querySelector("#round")
+
 
 billValue.addEventListener("change", changeTotal)
 tipValue.addEventListener("change", changeTotal)
 splitValue.addEventListener("change", updateSplit)
+roundBtn.addEventListener("click", roundNum)
 
-let total
+let total, splitted
 
 function changeTotal(){
     const bill = Number(billValue.value)
     const tip = Number(tipValue.value)
-    total = Math.round((bill*(1+tip/100))*100)/100
-    const temp = total.toString().split(".")
-    document.querySelector("#total").innerHTML = `${(temp.length == 1) ? `$${total}.00` : (temp[1].length === 1) ? `$${total}0` : `$${total}`}`
+    document.querySelector("#total").innerHTML = roundWithZero(bill*(1+tip/100))
 
-    splitBox.style.height = "20%"
+    splitBox.style.height = "22%"
     splitValue.value && updateSplit()
 }
 
 function updateSplit(){
-    document.querySelector("#split").innerHTML = `<strong>$${total/splitValue.value}</strong>`
+    splitted = total/splitValue.value
+    document.querySelector("#split").innerHTML = `<strong>$${splitted}</strong>`
+}
+
+function roundNum(){
+    console.log("clicked round")
+    if (splitted){
+        console.log("in")
+        document.querySelector("#split").innerHTML = `<strong>${roundWithZero(splitted)}</strong>`
+        console.log(roundBtn)
+        console.log(roundBtn.classList)
+        roundBtn.classList.toggle("clicked")
+        roundBtn.classList.toggle("float")
+    }
+}
+
+function roundWithZero(num){
+    total = Math.round(num*100)/100
+    const temp = total.toString().split(".")
+    return `${(temp.length == 1) ? `$${total}.00` : (temp[1].length === 1) ? `$${total}0` : `$${total}`}`
+}
+
+function addDigit(num){
+    
 }
