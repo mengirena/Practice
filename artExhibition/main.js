be3c2b3efbf66340e0f17e22157b0b3315d9d7cf
Irena Chen <irena1009@gmail.com>
2021-07-23 15:33:19 -0700

    create practice

--- artExhibition/main.js
+++
@@ -1,9 +0,0 @@
-const heart = document.getElementById("heart")
-const rect = document.getElementById("rect")
-
-heart.addEventListener("click", print)
-rect.addEventListener("click", print)
-
-function print(){
-    alert("clicked")
-}
