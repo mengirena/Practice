1e95513fe1cf7c88a0e61563d289ed3f27170c44
Irena Chen <irena1009@gmail.com>
2021-09-18 13:27:14 -0700

    save the scraped info to csv

--- web-scraping/scrape.js
+++ web-scraping/scrape.js
@@ -1,5 +1,10 @@
 const request = require("request")
 const cheerio = require("cheerio")
+const fs = require('fs')
+const writeStream = fs.createWriteStream('post.csv')
+
+//Write Headers
+writeStream.write(`各縣市政府自行車道路線資料下載 \n`)
 
 request('https://bikeway.cpami.gov.tw/Bikeway/CMMSER/DataShow.aspx?argNewsid=20210809134604707', (error, response, html) => {
     if (!error && response.statusCode == 200) {
@@ -21,7 +26,9 @@ request('https://bikeway.cpami.gov.tw/Bikeway/CMMSER/DataShow.aspx?argNewsid=202
         //     }
         // })
         siteHeading.each((i, elem ) => {
-            console.log($(elem).attr('href'))
+            writeStream.write(`${$(elem).attr('href')} \n`)
+
         })
+        console.log('done')
     }
 })
